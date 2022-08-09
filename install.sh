6c2e66de80ad74851d65d7e870399ba5104e179d
Marian Hahne <marian@marianhahne.de>
2022-08-09 16:18:40 +0200

    Add config for swaylock

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 source $HOME/.zshrc
 
 ln -s $DOTFILES/sway $XDG_CONFIG_HOME/sway
+ln -s $DOTFILES/swaylock $XDG_CONFIG_HOME/swaylock
 ln -s $DOTFILES/dunst $XDG_CONFIG_HOME/dunst
 ln -s $DOTFILES/alacritty $XDG_CONFIG_HOME/alacritty

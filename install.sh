c85b00d173c90d1b3c425bc195769649468c6c87
Marian Hahne <marian@marianhahne.de>
2022-08-11 15:46:55 +0200

    Add waybar config

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -s $DOTFILES/sway $XDG_CONFIG_HOME/sway
 ln -s $DOTFILES/swaylock $XDG_CONFIG_HOME/swaylock
 ln -s $DOTFILES/dunst $XDG_CONFIG_HOME/dunst
 ln -s $DOTFILES/alacritty $XDG_CONFIG_HOME/alacritty
+ln -s $DOTFILES/waybar $XDG_CONFIG_HOME/waybar

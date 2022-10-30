2976673b1a8107f497368cace5de5d6de07a8c9b
Marian Hahne <marian@marianhahne.de>
2022-10-30 18:02:35 +0100

    Add OS branching to install script

--- install.sh
+++ install.sh
@@ -3,10 +3,14 @@ ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.p10k.zsh $HOME/.p10k.zsh
 source $HOME/.zshrc
 
-ln -s $DOTFILES/sway $XDG_CONFIG_HOME/sway
-ln -s $DOTFILES/swaylock $XDG_CONFIG_HOME/swaylock
-ln -s $DOTFILES/dunst $XDG_CONFIG_HOME/dunst
-ln -s $DOTFILES/alacritty $XDG_CONFIG_HOME/alacritty
-ln -s $DOTFILES/waybar $XDG_CONFIG_HOME/waybar
+if [[ `uname` == "Darwin" ]]; then
+	
+elif [[ `uname` == "Linux" ]]; then
+	ln -s $DOTFILES/sway $XDG_CONFIG_HOME/sway
+    ln -s $DOTFILES/swaylock $XDG_CONFIG_HOME/swaylock
+    ln -s $DOTFILES/dunst $XDG_CONFIG_HOME/dunst
+    ln -s $DOTFILES/alacritty $XDG_CONFIG_HOME/alacritty
+    ln -s $DOTFILES/waybar $XDG_CONFIG_HOME/waybar
 
-ln -s $DOTFILES/systemd $XDG_CONFIG_HOME/systemd/user
+    ln -s $DOTFILES/systemd $XDG_CONFIG_HOME/systemd/user
+fi

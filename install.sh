557c0826f7a835eac80daf0e98887567601bfb9c
Marian Hahne <marian@marianhahne.de>
2022-11-05 17:29:01 +0100

    Add config for VSCode

--- install.sh
+++ install.sh
@@ -14,3 +14,15 @@ elif [[ `uname` == "Linux" ]]; then
 
     ln -s $DOTFILES/systemd $XDG_CONFIG_HOME/systemd/user
 fi
+
+# config for VSCode
+if [[ `uname` == "Darwin" ]]; then
+	VSC_BASE_PATH=${HOME}/Library/Application\ Support/Code
+elif [[ `uname` == "Linux" ]]; then
+    VSC_BASE_PATH=${XDG_CONFIG_HOME}/Code
+fi
+
+echo VSCode base path: $VSC_BASE_PATH
+ln -s $DOTFILES/vscode/settings.json $VSC_BASE_PATH/User/settings.json
+ln -s $DOTFILES/vscode/keybindings.json $VSC_BASE_PATH/User/keybindings.json
+ln -s $DOTFILES/vscode/snippets/ $VSC_BASE_PATH/User

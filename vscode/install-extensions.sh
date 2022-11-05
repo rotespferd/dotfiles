a8ad75564f2777cd5693be6707bc2910a7830a84
Marian Hahne <marian@marianhahne.de>
2022-11-05 19:35:34 +0100

    Add script to install vsc extensions

---
+++ vscode/install-extensions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Visual Studio Code :: Package list
+pkglist=(
+ms-python.python
+oderwat.indent-rainbow
+astro-build.astro-vscode
+coolbear.systemd-unit-file
+Equinusocio.vsc-community-material-theme
+ms-vscode-remote.remote-ssh
+ms-vscode-remote.remote-ssh-edit
+PKief.material-icon-theme
+rubenverg.bootstrap-product-icons
+)
+
+for i in ${pkglist[@]}; do
+  code --install-extension $i
+done
